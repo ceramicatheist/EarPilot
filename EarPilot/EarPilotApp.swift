bc48d6c59bda1c515fa41fd2c199c29df173c2a2
Rogers George <rgeorge@eclecticode.com>
2024-01-19 11:04:55 -0600

    Initial Commit

---
+++ EarPilot/EarPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarPilotApp.swift
+//  EarPilot
+//
+//  Created by Rogers George on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct EarPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
