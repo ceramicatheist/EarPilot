8efe10d637f254a393224c9391457cdfccd7095b
Rogers George <rgeorge@eclecticode.com>
2024-01-19 12:12:09 -0600

    basic attitude tracking and setting a zero reference text only

---
+++ EarPilot/FlightDisplay.swift
@@ -0,0 +1,18 @@
+//
+//  FlightDisplay.swift
+//  EarPilot
+//
+//  Created by Rogers George on 1/19/24.
+//
+
+import SwiftUI
+
+struct FlightDisplay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlightDisplay()
+}
---
+++ EarPilot/PositionTracker.swift
@@ -0,0 +1,8 @@
+//
+//  PositionTracker.swift
+//  EarPilot
+//
+//  Created by Rogers George on 1/19/24.
+//
+
+import Foundation
